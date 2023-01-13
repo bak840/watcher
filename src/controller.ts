1472e5671af1897568c120d113cf3c8a6485a053
Bakoura Ibrahima <bakouraibrahima@gmail.com>
2023-01-13 03:57:40 +0100

    Refacto

--- src/controller.ts
+++ src/controller.ts
@@ -7,40 +7,22 @@ class Controller {
     this.watches = [new Watch("Europe/Paris")];
   }
 
-  render(): string {
-    let result = `<h2>${this.watches.length} watches</h2>`;
-    this.watches.forEach((w) => (result = result + this.renderWatchToHTML(w)));
-    return result;
-  }
-
-  renderWatchToHTML(watch: Watch) {
-    let result = `<div id="${watch.id}" class="border">
-    <p class="${watch.on ? "watch-on" : "watch-off"}">${
-      watch.timezone
-    }: ${watch.time.toLocaleString("fr-FR", {
-      timeZone: watch.timezone,
-    })}</p>
-    <button id="lightButton">switch on/off</button>
-    </div>`;
-    return result;
+  dispatchRerenderEvent() {
+    document.dispatchEvent(new Event("rerender"));
   }
 
   getIndexOfWatchById(id: string) {
     return this.watches.findIndex((w) => w.id === id);
   }
 
-  switchWatchLight(id: string) {
-    this.watches[this.getIndexOfWatchById(id)].on =
-      !this.watches[this.getIndexOfWatchById(id)].on;
+  addWatch(locale: string) {
+    this.watches.push(new Watch(locale));
     this.dispatchRerenderEvent();
   }
 
-  dispatchRerenderEvent() {
-    document.dispatchEvent(new Event("rerender"));
-  }
-
-  addWatch(locale: string) {
-    this.watches.push(new Watch(locale));
+  switchWatchLight(id: string) {
+    const watchIndex = this.getIndexOfWatchById(id);
+    this.watches[watchIndex].on = !this.watches[watchIndex].on;
     this.dispatchRerenderEvent();
   }
 }
